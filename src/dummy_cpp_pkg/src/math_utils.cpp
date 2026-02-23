02730d4fe201494411d303c46019a58542033bc2
gurusarann <gurusarannavan@gmail.com>
2026-02-23 12:35:19 +0530

    divsion function

--- src/dummy_cpp_pkg/src/math_utils.cpp
+++ src/dummy_cpp_pkg/src/math_utils.cpp
@@ -14,4 +14,10 @@ double multiply(double a, double b)
 {
   return a * b;
 }
+double divide(double a, double b)
+{
+  if (b == 0) {
+    throw std::invalid_argument("Division by zero is not allowed.");
+  }
+  return a / b;
 }
